dd69cdf7ac4706290c93b044a842b0dc6d995a70
Dalia Younis <daliataysir@gmail.com>
2021-03-21 17:39:03 +0200

    add components to package

--- src/SpyAgent.php
+++ src/SpyAgent.php
@@ -1,37 +1,40 @@
 <?php
-
-
 namespace SpyAgent;
 require dirname(__DIR__, 1) . '\vendor\autoload.php';
 
 use Kreait\Firebase\Factory;
+use SpyAgent\managers\ProcessManager;
+use SpyAgent\managers\StepManager;
 
 class SpyAgent
 {
-    public static $firestore = null;
-    public static $documentProcessId = null;
+    public static $firestoreInstance = null;
+    public static $selectedSystem = null;
+    public static $createdAt = null;
+    public static $currentProcessDocumentId = null;
+    public static $activeProcess = null;
 
-    public static function init($serviceAccount)
+    public static function init(String $keyPath , String $selectedSystem)
     {
-        $factory = (new Factory())->withServiceAccount($serviceAccount);
-        self::$firestore = $factory->createFirestore()->database();
+        $factory = (new Factory())->withServiceAccount($keyPath);
+        self::$firestoreInstance = $factory->createFirestore()->database();
+        self::$selectedSystem = $selectedSystem;
+        self::$createdAt = time() ;
     }
 
-    public static function startProcess($processData = null)
+    public static function trackProcess(Process $aProcess)
     {
-
-        $aDocumentReference = self::$firestore->collection('systems')
-            ->document('PROPERTY_MANAGEMENT_SYSTEM');
-        $processesCollection = $aDocumentReference->collection('processes');
-        self::$documentProcessId = $processesCollection->add($processData)->id();
+        $aManager = new ProcessManager();
+        $aManager->trackProcess($aProcess);
     }
 
-    public static function stepCompleted($stepData = null)
+    public static function trackStep($stepName,Array $payload)
     {
-        $stepCollection = self::$firestore
-            ->collection('systems/PROPERTY_MANAGEMENT_SYSTEM/processes/' . self::$documentProcessId . '/steps');
-        $stepCollection->add($stepData);
+        $aManager = new StepManager();
+        $aManager->trackStep($stepName,$payload);
+    }
 
-        dd('Done');
+    public static function getActiveProcess(){
+        return self::$activeProcess;
     }
 }
